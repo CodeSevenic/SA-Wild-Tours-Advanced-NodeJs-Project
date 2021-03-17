c081f711f2e7468b52f68aea03b2cb596c7e8452
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-17 13:13:18 +0200

    stripe fixies

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -27,7 +27,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         images: [
           `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
         ],
-        // amount: tour.price * 100,
+        amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
       },
