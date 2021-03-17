d8f6471174a2087040c34c0a51631674c26226b0
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-17 11:28:13 +0200

    stripe fixies

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,14 +54,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
@@ -83,7 +82,7 @@ exports.webhookCheckout = (req, res, next) => {
         expand: ['line_items'],
       }
     );
-    
+
     const lineItems = session.line_items;
   }
   res.status(200).json({ received: true });
