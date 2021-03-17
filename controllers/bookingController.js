c81c86e414e22b0380e460cb0c933a811b5b5b0d
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-17 14:59:45 +0200

    stripe fixies

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,7 +51,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //     quantity: 1,
     //   },
     // ],
-    line_items: [{ price: tour.price, quantity: 1 }],
+    line_items: [{ price: `${tour.price * 100}`, quantity: 1 }],
     mode: 'payment',
   });
 
@@ -81,7 +81,7 @@ const createBookingCheckout = async (session) => {
 
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price;
+  const price = session.line_items[0].price / 100;
 
   await Booking.create({ tour, user, price });
 };
