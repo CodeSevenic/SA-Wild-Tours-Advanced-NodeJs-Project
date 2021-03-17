442f95ab7c919694871ff5844a2c130ffe665aab
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-17 12:25:52 +0200

    stripe fixies

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,6 +20,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    amount_subtotal: tour.price * 100,
     line_items: [
       {
         name: `${tour.name} Tour`,
@@ -56,7 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -76,11 +77,6 @@ exports.webhookCheckout = async (req, res, next) => {
 
   if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
-    const session = await stripe.checkout.sessions.retrieve(session.id, {
-      expand: ['line_items'],
-    });
-
-    session.line_items;
   }
   res.status(200).json({ received: true });
 };
